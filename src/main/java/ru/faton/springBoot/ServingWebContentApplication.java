228a924584e77a7ec4cf756e25d65055bc48574a
faton <aserg2013@mail.ru>
2020-03-21 12:34:46 +0700

    11

--- src/main/java/ru/faton/springBoot/ServingWebContentApplication.java
+++ src/main/java/ru/faton/springBoot/ServingWebContentApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServingWebContentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
+        //1111111111111111
     }
 }
